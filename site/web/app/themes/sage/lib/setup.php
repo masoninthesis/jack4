18a4760bc16c83c98be9d085935e100d05fd0f2e
Mason L <mason.lawlor@me.com>
2015-10-18 22:19:56 -0600

    SVGs are getting funky

--- site/web/app/themes/sage/lib/setup.php
+++ site/web/app/themes/sage/lib/setup.php
@@ -39,6 +39,7 @@ function setup() {
   // Enable post formats
   // http://codex.wordpress.org/Post_Formats
   add_theme_support('post-formats', ['aside', 'gallery', 'link', 'image', 'quote', 'video', 'audio']);
+  add_theme_support('post-formats', array('aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat'));
 
   // Enable HTML5 markup support
   // http://codex.wordpress.org/Function_Reference/add_theme_support#HTML5
@@ -85,6 +86,7 @@ function display_sidebar() {
     is_404(),
     is_front_page(),
     is_page_template('template-custom.php'),
+    ['is_page', 'sample-page']
   ]);
 
   return apply_filters('sage/display_sidebar', $display);
