cae7740d537cbed2af49d0a28cf2099d6d271b79
Mason L <mason.lawlor@me.com>
2015-10-18 23:57:11 -0600

    taking a break. Made it to section1, then the SVGs started falling apart.
    I’m going to see what I can do with the SVGOMG tool

---
+++ site/web/app/themes/sage/assets/svg/un-optimized/inline-ab-screens.svg.php
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- Generator: Adobe Illustrator 19.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="-14 130 312 84" style="enable-background:new -14 130 312 84;" xml:space="preserve">
+<style type="text/css">
+	.st0{clip-path:url(#SVGID_2_);fill:#FF6C6A;}
+	.st1{fill:#FF6C6A;}
+	.st2{font-family:'ProximaNova-Light';}
+	.st3{font-size:29.249px;}
+	.st4{fill:#3D3D3D;}
+	.st5{clip-path:url(#SVGID_4_);fill:#3D3D3D;}
+</style>
+<g id="TV_3_">
+	<g>
+		<defs>
+			<rect id="SVGID_1_" x="-13" y="130" width="106.9" height="84"/>
+		</defs>
+		<clipPath id="SVGID_2_">
+			<use xlink:href="#SVGID_1_"  style="overflow:visible;"/>
+		</clipPath>
+		<path class="st0" d="M86.3,130H-5.3c-4.1,0-7.6,3.4-7.6,7.6v61.1c0,4.2,3.5,7.6,7.6,7.6h45.8v3.8H13.8v3.8h57.3v-3.8H44.4v-3.8h42
+			c4.1,0,7.6-3.4,7.6-7.6v-61.1C93.9,133.4,90.4,130,86.3,130z M90.1,198.7c0,2.1-1.8,3.8-3.8,3.8H-5.3c-2.1,0-3.8-1.7-3.8-3.8
+			v-61.1c0-2.1,1.8-3.8,3.8-3.8h91.6c2.1,0,3.8,1.7,3.8,3.8L90.1,198.7L90.1,198.7z"/>
+	</g>
+</g>
+<text transform="matrix(1 0 0 1 32.42 176.876)" class="st1 st2 st3">A</text>
+<text transform="matrix(1 0 0 1 238.42 176.876)" class="st4 st2 st3">B</text>
+<g id="TV_2_">
+	<g>
+		<defs>
+			<rect id="SVGID_3_" x="192" y="129" width="106.9" height="84"/>
+		</defs>
+		<clipPath id="SVGID_4_">
+			<use xlink:href="#SVGID_3_"  style="overflow:visible;"/>
+		</clipPath>
+		<path class="st5" d="M291.3,129h-91.6c-4.1,0-7.6,3.4-7.6,7.6v61.1c0,4.2,3.5,7.6,7.6,7.6h45.8v3.8h-26.7v3.8H276v-3.8h-26.7v-3.8
+			h42c4.1,0,7.6-3.4,7.6-7.6v-61.1C298.9,132.4,295.4,129,291.3,129z M295.1,197.7c0,2.1-1.8,3.8-3.8,3.8h-91.6
+			c-2.1,0-3.8-1.7-3.8-3.8v-61.1c0-2.1,1.8-3.8,3.8-3.8h91.6c2.1,0,3.8,1.7,3.8,3.8C295.1,136.6,295.1,197.7,295.1,197.7z"/>
+	</g>
+</g>
+</svg>
---
+++ site/web/app/themes/sage/assets/svg/un-optimized/inline-plus-sign.svg.php
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- Generator: Adobe Illustrator 19.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="-150 152 40 40" style="enable-background:new -150 152 40 40;" xml:space="preserve">
+<style type="text/css">
+	.st0{fill:#3D3D3D;}
+</style>
+<g id="Plus_6_">
+	<path class="st0" d="M-130,153.3c10.3,0,18.7,8.4,18.7,18.7s-8.4,18.7-18.7,18.7s-18.7-8.4-18.7-18.7S-140.3,153.3-130,153.3
+		 M-130,152c-11,0-20,9-20,20s9,20,20,20s20-9,20-20S-119,152-130,152z"/>
+	<path class="st0" d="M-118,171.2h-11.2V160h-1.6v11.2H-142v1.6h11.2V184h1.6v-11.2h11.2C-118,172.8-118,171.2-118,171.2z"/>
+</g>
+</svg>
