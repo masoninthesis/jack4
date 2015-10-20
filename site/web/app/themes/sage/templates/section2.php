7c7837c8997f3a6fd00bebf9b77cacdad14c266e
Mason L <mason.lawlor@me.com>
2015-10-20 00:34:48 -0600

    end of screencast 7.2-phase1.2

--- site/web/app/themes/sage/templates/section2.php
+++ site/web/app/themes/sage/templates/section2.php
@@ -3,14 +3,60 @@
     <h2><strong>Great websites aren't built overnight.</strong></h2>
     <h3>Your site is aged to perfection over 12 months.</h3>
     <div class="text-center">
-      <svg width="771" height="271"><?php get_template_part( 'assets/svg/inline', 'phase-one.svg' ); ?></svg>
+      <svg class="ph1-schedule" width="771" height="271"><?php get_template_part( 'assets/svg/inline', 'phase-one.svg' ); ?></svg>
     </div>
     <div class="container">
       <p>Your billing cycle starts <u>not a second before your site goes live</u> for testing. Over the next 4 months we prioritize testing content and design variations.</p>
-      <h5>3rd Party Tools Included:</h5>
+      <h5 class="3pt">3rd Party Tools Included:</h5>
       <p class="p-sm">DigitalOcean, Mailchimp, Google Adwords/Analytics, Slack, Github, CrazyEgg, Sass, Bootstrap, HTML5, and CSS3</p>
       <svg class="tools" width="758" height="152"><?php get_template_part( 'assets/svg/inline', 'phase-one-tools.svg' ); ?></svg>
       <button name="button" class="btn btn-lg jack-btn1 primary-gradient-stroke" >Learn more about our tools<br>and how we use them.</button>
     </div>
+
+    <div class="" id="phase-one-bullets">
+      <div class="container">
+        <div class="row top-row">
+          <div class="bullets col-sm-6">
+            <div class="col-xs-2">
+              <svg width="34" height="36"><?php get_template_part( 'assets/svg/inline', 'calendar.svg' ); ?></svg>
+            </div>
+            <div class="col-xs-10">
+              <h5>One Split-test Per Week</h5>
+              <p>So we don’t get distracted, let’s focus on improving just one metric each week.</p>
+            </div>
+          </div>
+          <div class="bullets col-sm-6">
+            <div class="col-xs-2">
+              <svg width="34" height="36"><?php get_template_part( 'assets/svg/inline', 'calendar.svg' ); ?></svg>
+            </div>
+            <div class="col-xs-10">
+              <h5>One Split-test Per Week</h5>
+              <p>So we don’t get distracted, let’s focus on improving just one metric each week.</p>
+            </div>
+          </div>
+        </div>
+        <div class="row bottom-row">
+          <div class="bullets col-sm-6">
+            <div class="col-xs-2">
+              <svg width="34" height="36"><?php get_template_part( 'assets/svg/inline', 'calendar.svg' ); ?></svg>
+            </div>
+            <div class="col-xs-10">
+              <h5>One Split-test Per Week</h5>
+              <p>So we don’t get distracted, let’s focus on improving just one metric each week.</p>
+            </div>
+          </div>
+          <div class="bullets col-sm-6">
+            <div class="col-xs-2">
+              <svg width="34" height="36"><?php get_template_part( 'assets/svg/inline', 'calendar.svg' ); ?></svg>
+            </div>
+            <div class="col-xs-10">
+              <h5>One Split-test Per Week</h5>
+              <p>So we don’t get distracted, let’s focus on improving just one metric each week.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </div>
