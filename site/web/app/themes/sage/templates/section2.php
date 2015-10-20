3019edba527306a393ce71a3bf0977347dc86ca7
Mason L <mason.lawlor@me.com>
2015-10-20 03:08:28 -0600

    screencast 7.3? right before tying in all new svgs

--- site/web/app/themes/sage/templates/section2.php
+++ site/web/app/themes/sage/templates/section2.php
@@ -1,4 +1,6 @@
 <div id="section2">
+  <!-- Phase One -->
+  <!--           -->
   <div class="phase-one">
     <h2><strong>Great websites aren't built overnight.</strong></h2>
     <h3>Your site is aged to perfection over 12 months.</h3>
@@ -18,7 +20,7 @@
         <div class="row top-row">
           <div class="bullets col-sm-6">
             <div class="col-xs-2">
-              <svg width="34" height="36"><?php get_template_part( 'assets/svg/inline', 'calendar.svg' ); ?></svg>
+              <svg class="calendar" width="34" height="36"><?php get_template_part( 'assets/svg/inline', 'calendar.svg' ); ?></svg>
             </div>
             <div class="col-xs-10">
               <h5>One Split-test Per Week</h5>
@@ -27,31 +29,151 @@
           </div>
           <div class="bullets col-sm-6">
             <div class="col-xs-2">
-              <svg width="34" height="36"><?php get_template_part( 'assets/svg/inline', 'calendar.svg' ); ?></svg>
+              <svg class="long-short" width="34" height="36"><?php get_template_part( 'assets/svg/inline', 'long-short.svg' ); ?></svg>
             </div>
             <div class="col-xs-10">
-              <h5>One Split-test Per Week</h5>
-              <p>So we don’t get distracted, let’s focus on improving just one metric each week.</p>
+              <h5>Long Form vs. Short Form</h5>
+              <p>Let’s test how quickly we should deliver the message.</p>
             </div>
           </div>
         </div>
         <div class="row bottom-row">
           <div class="bullets col-sm-6">
             <div class="col-xs-2">
-              <svg width="34" height="36"><?php get_template_part( 'assets/svg/inline', 'calendar.svg' ); ?></svg>
+              <svg class="user-centered" width="34" height="36"><?php get_template_part( 'assets/svg/inline', 'user-center.svg' ); ?></svg>
             </div>
             <div class="col-xs-10">
-              <h5>One Split-test Per Week</h5>
-              <p>So we don’t get distracted, let’s focus on improving just one metric each week.</p>
+              <h5>“User-centering” The Offer</h5>
+              <p>Let’s develop a detailed user persona for your customers.</p>
             </div>
           </div>
           <div class="bullets col-sm-6">
             <div class="col-xs-2">
-              <svg width="34" height="36"><?php get_template_part( 'assets/svg/inline', 'calendar.svg' ); ?></svg>
+              <svg class="variations" width="34" height="36"><?php get_template_part( 'assets/svg/inline', 'variations.svg' ); ?></svg>
             </div>
             <div class="col-xs-10">
-              <h5>One Split-test Per Week</h5>
-              <p>So we don’t get distracted, let’s focus on improving just one metric each week.</p>
+              <h5>UX/Design Variations</h5>
+              <p>Once content has been tested, we’ll dig deeper into your funnel, UX, and design.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+  <!-- Phase Two -->
+  <!--           -->
+  <div class="phase-two">
+    <div class="text-center">
+      <svg class="ph1-schedule" width="771" height="271"><?php get_template_part( 'assets/svg/inline', 'phase-one.svg' ); ?></svg>
+    </div>
+    <div class="container">
+      <p>Once you’ve made it to Phase Two, you unlock our custom built Wordpress theme. The testing never stops, but now that we’ve optimized the content and design, it’s time to expand the backend– giving you the most powerful CMS in the world.</p>
+      <p>The key is to do this without sacrificing load speed– <strong>this is our specialty.</strong></p>
+      <h5 class="3pt">Additional Tools:</h5>
+      <p class="p-sm">Wordpress, Bedrock, Sage, Trellis (Vagrant + Ansible), and Gravity Forms</p>
+      <svg class="tools" width="758" height="152"><?php get_template_part( 'assets/svg/inline', 'phase-one-tools.svg' ); ?></svg>
+    </div>
+
+    <div class="" id="phase-one-bullets">
+      <div class="container">
+        <div class="row top-row">
+          <div class="bullets col-sm-6">
+            <div class="col-xs-2">
+              <svg class="calendar" width="34" height="36"><?php get_template_part( 'assets/svg/inline', 'calendar.svg' ); ?></svg>
+            </div>
+            <div class="col-xs-10">
+              <h5>Custom Wordpress Theme</h5>
+              <p>We only develop on Sage, the most advanced WP theme in the world.</p>
+            </div>
+          </div>
+          <div class="bullets col-sm-6">
+            <div class="col-xs-2">
+              <svg class="long-short" width="34" height="36"><?php get_template_part( 'assets/svg/inline', 'long-short.svg' ); ?></svg>
+            </div>
+            <div class="col-xs-10">
+              <h5>Increased Load Speed</h5>
+              <p>Your load speed will actually be faster on Wordpress.</p>
+            </div>
+          </div>
+        </div>
+        <div class="row bottom-row">
+          <div class="bullets col-sm-6">
+            <div class="col-xs-2">
+              <svg class="user-centered" width="34" height="36"><?php get_template_part( 'assets/svg/inline', 'user-center.svg' ); ?></svg>
+            </div>
+            <div class="col-xs-10">
+              <h5>Access to Powerful WP Plugins</h5>
+              <p>ou’ll now have access to plugins like GravityForms, Soil, and Optimizely.</p>
+            </div>
+          </div>
+          <div class="bullets col-sm-6">
+            <div class="col-xs-2">
+              <svg class="variations" width="34" height="36"><?php get_template_part( 'assets/svg/inline', 'variations.svg' ); ?></svg>
+            </div>
+            <div class="col-xs-10">
+              <h5>Automatic Database Backup</h5>
+              <p>With Trellis, we automatically backup your database with version-control.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+  <!-- Phase Three -->
+  <!--           -->
+  <div class="phase-three">
+    <div class="text-center">
+      <svg class="ph1-schedule" width="771" height="271"><?php get_template_part( 'assets/svg/inline', 'phase-one.svg' ); ?></svg>
+    </div>
+    <div class="container">
+      <p>Split-testing and backend optimization continue, but Phase 3 is all about the power of content marketing. During Phase 3 we will produce 1 blog post, 1 video, 1 podcast episode, or 1 info-graphic for you a week.</p>
+      <p><strong>Now, it’s time to build some content.</strong></p>
+      <h2><strong>We guarantee 10k+ visitors per month.</strong></h2>
+    </div>
+
+    <div class="" id="phase-one-bullets">
+      <div class="container">
+        <div class="row top-row">
+          <div class="bullets col-sm-6">
+            <div class="col-xs-2">
+              <svg class="calendar" width="34" height="36"><?php get_template_part( 'assets/svg/inline', 'calendar.svg' ); ?></svg>
+            </div>
+            <div class="col-xs-10">
+              <h5>One 750+ Word Blog Post/wk</h5>
+              <p>Published to your blog, medium, or any guest blog.</p>
+            </div>
+          </div>
+          <div class="bullets col-sm-6">
+            <div class="col-xs-2">
+              <svg class="long-short" width="34" height="36"><?php get_template_part( 'assets/svg/inline', 'long-short.svg' ); ?></svg>
+            </div>
+            <div class="col-xs-10">
+              <h5>One 30+ Second Video/wk</h5>
+              <p>Published to Vimeo, Youtube, and your personal blog using Wistia.</p>
+            </div>
+          </div>
+        </div>
+        <div class="row bottom-row">
+          <div class="bullets col-sm-6">
+            <div class="col-xs-2">
+              <svg class="user-centered" width="34" height="36"><?php get_template_part( 'assets/svg/inline', 'user-center.svg' ); ?></svg>
+            </div>
+            <div class="col-xs-10">
+              <h5>One 2+ Minute Podcast Ep/wk</h5>
+              <p>Published to Soundclound, iTunes, Stitcher, and Youtube.</p>
+            </div>
+          </div>
+          <div class="bullets col-sm-6">
+            <div class="col-xs-2">
+              <svg class="variations" width="34" height="36"><?php get_template_part( 'assets/svg/inline', 'variations.svg' ); ?></svg>
+            </div>
+            <div class="col-xs-10">
+              <h5>One Infographic/wk</h5>
+              <p>Published to a guest blog or your personal blog.</p>
             </div>
           </div>
         </div>
